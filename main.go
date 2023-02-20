69f3d34860dc23ec0b3e19630c914d71c7394879
Vadim <shultzenfegel@gmail.com>
2023-02-20 23:51:28 +0500

    jaeger

--- main.go
+++ main.go
@@ -1,13 +1,64 @@
 package main
 
 import (
+	"context"
+	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	stdout "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
+const (
+	service     = "x-demo-traces"
+	environment = "production"
+	id          = 1
+)
+
+var tracerProvider *sdktrace.TracerProvider
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func main() {
+
+	// tp, err := initStdoutTracer()
+	tp, err := initJaegerTracer("http://localhost:14268/api/traces")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tracerProvider = tp
+	otel.SetTracerProvider(tracerProvider)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Cleanly shutdown and flush telemetry when the application exits.
+	defer func(ctx context.Context) {
+		// Do not make the application hang when it is shutdown.
+		ctx, cancel = context.WithTimeout(ctx, time.Second*5)
+		defer cancel()
+		if err := tracerProvider.Shutdown(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}(ctx)
+
 	router := gin.Default()
+	router.Use(otelgin.Middleware("x-demo-traces-server"))
+
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
@@ -32,7 +83,53 @@ var albums = []album{
 
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
+	tr := tracerProvider.Tracer("getAlbums")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ctx, span := tr.Start(ctx, "getAlbums_span")
+	defer span.End()
+
+	randomSleep(ctx)
+	getAlbumsInternal(ctx)
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func randomSleep(ctx context.Context) {
+	n := rand.Intn(1000)
+
+	tr := otel.Tracer("randomSleep")
+	_, span := tr.Start(ctx, "randomSleep_span")
+	span.SetAttributes(attribute.Key("sleep_time").Int(n))
+	defer span.End()
+
+	time.Sleep(time.Duration(n) * time.Millisecond)
+}
+
+func getAlbumsInternal(ctx context.Context) []album {
+	tr := otel.Tracer("getAlbumsInternal")
+	_, span := tr.Start(ctx, "getAlbumsInternal_span")
+	defer span.End()
+
+	n := rand.Intn(1000)
+	time.Sleep(time.Duration(n) * time.Millisecond)
+
+	return albums
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 // postAlbums adds an album from JSON received in the request body.
@@ -50,18 +147,44 @@ func postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
+// https://github.com/open-telemetry/opentelemetry-go-contrib/blob/main/instrumentation/github.com/gin-gonic/gin/otelgin/example/server.go
 
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+func initStdoutTracer() (*sdktrace.TracerProvider, error) {
+	exporter, err := stdout.New(stdout.WithPrettyPrint())
+	if err != nil {
+		return nil, err
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exporter),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp, nil
+}
+
+// https://github.com/open-telemetry/opentelemetry-go/blob/main/example/jaeger/main.go
+
+// initJaegerTracer returns an OpenTelemetry TracerProvider configured to use
+// the Jaeger exporter that will send spans to the provided url. The returned
+// TracerProvider will also use a Resource configured with all the information
+// about the application.
+func initJaegerTracer(url string) (*sdktrace.TracerProvider, error) {
+	// Create the Jaeger exporter
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	tp := sdktrace.NewTracerProvider(
+		// Always be sure to batch in production.
+		sdktrace.WithBatcher(exp),
+		// Record information about this application in a Resource.
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(service),
+			attribute.String("environment", environment),
+			attribute.Int64("ID", id),
+		)),
+	)
+	return tp, nil
 }
